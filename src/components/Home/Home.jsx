f4ebb55b8c3fb5a20999d696020b0b86c834a5ae
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2024-04-05 23:29:04 +0300

    update the GYm

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,22 +7,22 @@ const Home = () => {
     <section className='Home_container'>
         <div className='Home_Content'>
             <h1><span>THE BEST</span>FITNESS CLUB</h1>
-            <h2>Work Hard To Get Better Life</h2>
+            <h2>Work Hard To <br /> Get Better Life</h2>
             <p>Duis mollis felis quis libero dictum vehicula. Duis dictum lorem mi, a faucibus nisi eleifend eu.</p>
             <a href='#' className='Home_btn'>Get Started</a>
         </div>
         <div className='Home_image'>
+          <div className='Calorie'>
+            <FaHeart />
+            <p>Calorie burned</p>
+            <p>1000 cal</p>
+          </div>
+          <div className='HeartRate'>
+            <FaHeart />
+            <p>Heart Rate</p>
+            <p>100bpm</p>
+          </div>
             <img src="https://www.pngarts.com/files/6/Male-Fitness-PNG-Transparent-Image.png" alt=""/>
-            <div className='Calorie'>
-              <FaHeart />
-              <p>Calorie Burned</p>
-              <h1>100Cal</h1>
-            </div>
-            <div className='HeartRate'>
-              <FaHeart />
-              <p>Heart Rate</p>
-              <h1>100bpm</h1>
-            </div>
         </div>
     </section>
   )
